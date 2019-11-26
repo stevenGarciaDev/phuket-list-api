b55695b362697494b0ad0eb744812154dc7f15e1
SukritMehraCsulb <sukritmehra690@gmail.com>
2019-11-26 11:49:59 -0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.send(400).send('Invalid email or password');
+  if (!user) return res.send(401).send('user Does not exist');
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password.');
