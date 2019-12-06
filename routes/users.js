c523b605b862392294355b554da9e6709648ae44
SukritMehraCsulb <sukritmehra690@gmail.com>
2019-12-05 18:35:44 -0800

    Updated Reset password link
    changed ink from old websiteto new website

--- routes/users.js
+++ routes/users.js
@@ -233,7 +233,7 @@ router.post('/forgotPassword', async (req,  res)=> {
       text:
         'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
         + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
-        + `https://phuketlist.herokuapp.com/reset/${token}\n\n`
+        + `https://phuket-list.herokuapp.com/reset/${token}\n\n`
         + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
     };
     console.log('sending mail');
